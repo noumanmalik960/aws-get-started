e57e4203f6236231a104a4a30da9c086e8b959c8
Nouman Javed <noumanmalik960@gmail.com>
2020-12-26 01:32:27 +0500

    without authorization

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,16 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    // here bucket is a contruct, all constructs take 3 params, 3rd being optional
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
